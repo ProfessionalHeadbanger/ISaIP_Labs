28cbf02156d71cb42112d5aab5802b6f18746e44
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2024-10-25 14:45:47 +0300

    Add files via upload

---
+++ ISaIP_Lab2/ISaIP_Lab2/Source.cpp
@@ -0,0 +1,77 @@
+// Вариант 4
+// key = EARTH
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+class VigenereCipher
+{
+public:
+    VigenereCipher(int capacity, std::string key) : capacity(capacity), key(key) {}
+
+    std::string encrypt(const std::string& text)
+    {
+        std::string encryptedText;
+        for (size_t i = 0; i < text.length(); i++)
+        {
+            char letter = toupper(text[i]);
+            char keyLetter = toupper(key[i % key.length()]);
+
+            if (isalpha(letter))
+            {
+                char encryptedChar = (letter + keyLetter - 2 * 'A') % 26 + 'A';
+                encryptedText += encryptedChar;
+            }
+            else
+            {
+                encryptedText += letter;
+            }
+        }
+        return encryptedText;
+    }
+
+    std::string decrypt(const std::string& encryptedText)
+    {
+        std::string decryptedText;
+        for (size_t i = 0; i < encryptedText.length(); i++)
+        {
+            char letter = toupper(encryptedText[i]);
+            char keyLetter = toupper(key[i % key.length()]);
+
+            if (isalpha(letter))
+            {
+                char decryptedChar = (letter + 26 - keyLetter) % 26 + 'A';
+                decryptedText += decryptedChar;
+            }
+            else
+            {
+                decryptedText += letter;
+            }
+        }
+        return decryptedText;
+    }
+private:
+    int capacity;
+    std::string key;
+};
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int capacity = 26;
+    std::string text, key;
+    std::cout << "Введите текст для шифрования: ";
+    std::getline(std::cin, text);
+    std::cout << "Введите ключ: ";
+    std::getline(std::cin, key);
+
+    VigenereCipher cipher(capacity, key);
+
+    std::string encryptedText = cipher.encrypt(text);
+    std::cout << "Зашифрованный текст: " << encryptedText << std::endl;
+
+    std::string decryptedText = cipher.decrypt(encryptedText);
+    std::cout << "Расшифрованный текст: " << decryptedText << std::endl;
+}
